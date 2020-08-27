8e1429e63bb47847f74d444cdf9c142e1f06c13a
Ameya Ganore <ameyaganore@gmail.com>
2020-08-27 23:50:55 +0530

    Updates

---
+++ precache-manifest.3b3b63cbe28f058c3ff890ea86cd93ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43bd57ae790349977e90b4fe84bc9540",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2514335e57781a204666",
+    "url": "/monster-rolodex/static/css/main.a62fd009.chunk.css"
+  },
+  {
+    "revision": "72310d03762a86455f9f",
+    "url": "/monster-rolodex/static/js/2.6e33a265.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.6e33a265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2514335e57781a204666",
+    "url": "/monster-rolodex/static/js/main.25bbcbd8.chunk.js"
+  },
+  {
+    "revision": "20b39983d3692c6258f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a349263.js"
+  }
+]);
